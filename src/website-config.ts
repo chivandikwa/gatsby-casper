af97ed7f894c577b445d78eb8d37d823e68a58ff
Thulani Chivandikwa <chivandikwa.t@gmail.com>
2021-01-30 20:57:16 +0100

    4

--- src/website-config.ts
+++ src/website-config.ts
@@ -57,8 +57,9 @@ const config: WebsiteConfig = {
   siteUrl: 'https://gatsby-casper.netlify.com',
   facebook: 'https://www.facebook.com/ghost',
   twitter: 'https://twitter.com/tryghost',
-  showSubscribe: true,
-  mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
+  showSubscribe: false,
+  mailchimpAction:
+    'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
   mailchimpEmailFieldName: 'MERGE0',
   googleSiteVerification: 'GoogleCode',
