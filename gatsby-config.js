70df1881aa6134569198ad734d8e3b5516ef4337
Thulani Chivandikwa <chivandikwa.t@gmail.com>
2021-01-30 21:56:14 +0100

    fix plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,25 +25,6 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-remark-vscode',
-      // All options are optional. Defaults shown here.
-      options: {
-        colorTheme: 'Dark+ (default dark)', // Read on for list of included themes. Also accepts object and function forms.
-        wrapperClassName: '', // Additional class put on 'pre' tag
-        injectStyles: true, // Injects (minimal) additional CSS for layout and scrolling
-        extensions: [], // Extensions to download from the marketplace to provide more languages and themes
-        languageAliases: {}, // Map of custom/unknown language codes to standard/known language codes
-        replaceColor: x => x, // Function allowing replacement of a theme color with another. Useful for replacing hex colors with CSS variables.
-        getLineClassName: ({
-          // Function allowing dynamic setting of additional class names on individual lines
-          content, //   - the string content of the line
-          index, //   - the zero-based index of the line within the code fence
-          language, //   - the language specified for the code fence
-          codeFenceOptions, //   - any options set on the code fence alongside the language (more on this later)
-        }) => '',
-      },
-    },
-    {
       resolve: 'gatsby-remark-emojis',
       options: {
         // Deactivate the plugin globally (default: true)
@@ -82,6 +63,12 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
+          {
+            resolve: 'gatsby-remark-vscode',
+            options: {
+              theme: 'Dark+ (default dark)', // Or install your favorite theme from GitHub
+            },
+          },
           {
             resolve: 'gatsby-remark-responsive-iframe',
             options: {
